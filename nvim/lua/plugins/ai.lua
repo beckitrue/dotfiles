14f81261f90e0c6b34c4d3774ae37a7ab54b2867
beckitrue <3496219+beckitrue@users.noreply.github.com>
2024-04-07 11:47:21 -0500

    removing files after updating with stow

--- nvim/lua/plugins/ai.lua
+++
@@ -1,4 +0,0 @@
-require('gen').prompts['DevOps me!'] = {
-  prompt = "You are a senior security devops engineer, acting as an assistant. You offer to help with cloud technologies like: Terraform, AWS, kubernetes, python, go, IAM. You answer with code examples when possible. $input:\n$text",
-  replace = true
-}
