9c74fdfc546e6c8205bf7775ce44783a07374efc
beckitrue <3496219+beckitrue@users.noreply.github.com>
2024-04-14 14:23:08 -0500

    added noice suggested options

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -125,6 +125,14 @@ require('lazy').setup({
     config = function()
       require("noice").setup({
         -- add any options here
+        lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
         presets = {
           command_palette = true,
 		  long_message_to_split = true,
