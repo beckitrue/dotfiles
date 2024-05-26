0240867d518d564f75c4c9337fc9fddb0c29a374
beckitrue <3496219+beckitrue@users.noreply.github.com>
2024-05-26 15:32:22 -0500

    fixed obsidian configuration to match new specs

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -2,12 +2,22 @@ require("obsidian").setup({
   workspaces = {
     {
       name = "Notes",
-      path = "/home/becki/notes/Notes",
+      path = "/home/becki/Notes",
     },
   },
   daily_notes = {
     folder = "journal",
   },
+  new_notes_location = "notes_subdir",
+  wiki_link_func = function(opts)
+    if opts.id == nil then
+      return string.format("[[%s]]", opts.label)
+    elseif opts.label ~= opts.id then
+      return string.format("[[%s|%s]]", opts.id, opts.label)
+    else
+      return string.format("[[%s]]", opts.id)
+    end
+  end,
   completion = {
     -- Set to false to disable completion.
     nvim_cmp = true,
@@ -18,8 +28,6 @@ require("obsidian").setup({
     -- Where to put new notes created from completion. Valid options are
     --  * "current_dir" - put new notes in same directory as the current buffer.
     --  * "notes_subdir" - put new notes in the default notes subdirectory.
-    new_notes_location = "notes_subdir",
-
     -- Control how wiki links are completed with these (mutually exclusive) options:
     --
     -- 1. Whether to add the note ID during completion.
